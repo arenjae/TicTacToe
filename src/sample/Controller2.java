4b07eaba5ba83b5fad174408cf596a65eb5d13ab
earthshine0 <earthshinelynx@gmail.com>
2015-03-23 22:57:25 -0700

    Fixed bugs with switching Scenes

--- src/sample/Controller2.java
+++ src/sample/Controller2.java
@@ -1,14 +1,6 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
